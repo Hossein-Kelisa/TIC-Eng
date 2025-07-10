7fcffe59fff01af47f77c47fa2b938703024462f
Hossein <Hossein.kelisa@gmail.com>
2025-07-10 12:40:55 +0200

    initialize recat-vite

---
+++ tic-eng/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <section>
+        <h1>Test, Inspection, Control</h1>
+        <p>welcome to our webpage</p>
+      </section>
+
+      <section>
+        <h2>Our Services</h2>
+        <ul>
+          <li>Hossein</li>
+          <li>Shokri</li>
+          <li>Kelisa</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>About Us</h2>
+        <p>
+          We are TIC Group
+        </p>
+      </section>
+
+      <footer style={{ marginTop: "50px", textAlign: "center" }}>
+        <p>&copy; 2025 TIC Quality</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
