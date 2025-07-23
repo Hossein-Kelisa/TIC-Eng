719465cd0101584bd44cd1f855668bff26b5399b
Hossein <Hossein.kelisa@gmail.com>
2025-07-23 22:57:43 +0200

    using cloudinary to hero images

--- src/Data/heroImages.js
+++ src/Data/heroImages.js
@@ -1,13 +1,12 @@
 const heroImages = [
-    "https://res.cloudinary.com/dk9rkpvhm/image/upload/neil-bowness-y1YC3itCSWQ-unsplash_dqjwpi.jpg",
-    "https://res.cloudinary.com/dk9rkpvhm/image/upload/syd-mills-Eq-bV3cbsro-unsplash_v52pwe.jpg",
-    "https://res.cloudinary.com/dk9rkpvhm/image/upload/Screenshot_2025-07-15_at_12.59.06_i2lspm.jpg",
-    "https://res.cloudinary.com/dk9rkpvhm/image/upload/lenny-kuhne-jHZ70nRk7Ns-unsplash_v6pmwb.jpg",
-    "https://res.cloudinary.com/dk9rkpvhm/image/upload/rob-lambert-9Q_pLLP_jmA-unsplash_znyb76.jpg",
-    "https://res.cloudinary.com/dk9rkpvhm/image/upload/pete-wright-n1RJ7pXgGTE-unsplash_jz6vuk.jpg",
-    "https://res.cloudinary.com/dk9rkpvhm/image/upload/heber-davis-E15BQufHzJs-unsplash_iol4pf.jpg",
-    "https://res.cloudinary.com/dk9rkpvhm/image/upload/benjamin-wedemeyer-bzzSg8hTKKY-unsplash_ramt2q.jpg",
-    "https://res.cloudinary.com/dk9rkpvhm/image/upload/ildefonso-polo-DX9X0g0Cg88-unsplash_a0mxck.jpg"
+    "https://res.cloudinary.com/dk9rkpvhm/image/upload/1_trkwiy.jpg",
+    "https://res.cloudinary.com/dk9rkpvhm/image/upload/2_zd1krr.jpg",
+    "https://res.cloudinary.com/dk9rkpvhm/image/upload/3_r1ucwj.jpg",
+    "https://res.cloudinary.com/dk9rkpvhm/image/upload/4_cjyxmb.jpg",
+    "https://res.cloudinary.com/dk9rkpvhm/image/upload/5_rcuklu.jpg",
+    "https://res.cloudinary.com/dk9rkpvhm/image/upload/6_nxxxp0.jpg",
+    "https://res.cloudinary.com/dk9rkpvhm/image/upload/7_mgukmh.jpg",
+    "https://res.cloudinary.com/dk9rkpvhm/image/upload/8_mojoqr.jpg"
 ];
 
 export default heroImages;
