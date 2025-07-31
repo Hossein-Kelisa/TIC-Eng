a4487601f99246648b216bdfa05ff04cd70218f3
Hossein <Hossein.kelisa@gmail.com>
2025-07-31 22:03:52 +0200

    add homePage

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+import About from '../components/About';
+import Industries from '../components/Industries';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+
+function HomePage() {
+  return (
+    <main>
+      <Header />
+      <Hero />
+      <Services />
+      <About />
+      <Industries />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
+
+export default HomePage;
