9156659f59fb53ad9f7312fcc267e2be994cc646
Hossein <Hossein.kelisa@gmail.com>
2025-08-13 23:52:14 +0200

    initilizeBackend

---
+++ Back-End/models/ServiceRequest.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const serviceRequestSchema = new mongoose.Schema({
+  customerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  serviceType: {
+    type: String,
+    required: true,
+  },
+  details: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('ServiceRequest', serviceRequestSchema);
