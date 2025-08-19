0c292e966b2d912769190a6d3b0b94b25d362505
Hossein <Hossein.kelisa@gmail.com>
2025-08-19 22:23:40 +0200

    authorisation page

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,6 +1,6 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AuthPage from "./Pages/AuthPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
