ec2df9a21aac3eeee7abc334dc2146475aa08aed
Hossein <Hossein.kelisa@gmail.com>
2025-08-20 01:34:15 +0200

    user register and login

---
+++ Back-End/middlewares/validateRegister.js
@@ -0,0 +1,24 @@
+// Back-End/middleware/validateRegister.js
+import { body, validationResult } from "express-validator";
+
+export const validateRegister = [
+  body("name").trim().notEmpty().withMessage("Name is required"),
+  body("email").isEmail().withMessage("Valid email is required").normalizeEmail(),
+  body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) throw new Error("Passwords do not match");
+    return true;
+  }),
+
+  // Final error handler
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "Validation failed",
+        errors: errors.array(),
+      });
+    }
+    next();
+  },
+];
--- Back-End/routes/authRoutes.js
+++ Back-End/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { registerUser, loginUser, getMe } from '../controllers/authController.js';
 import { protect } from '../middlewares/authMiddleware.js';
+import { validateRegister } from '../middlewares/validateRegister.js';
 
 const authRouter = express.Router();
 
-authRouter.post('/register', registerUser);
+authRouter.post('/register', validateRegister, registerUser);
 authRouter.post('/login', loginUser);
 authRouter.get('/me', protect, getMe);
 
