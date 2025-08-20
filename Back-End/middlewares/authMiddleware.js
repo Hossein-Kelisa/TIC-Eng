e194251f7f1087268adb73417f2a51ce02063c82
Hossein <Hossein.kelisa@gmail.com>
2025-08-20 02:04:54 +0200

    user register and login

--- Back-End/middlewares/authMiddleware.js
+++ Back-End/middlewares/authMiddleware.js
@@ -2,14 +2,26 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 export const protect = async (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) return res.status(401).json({ message: 'Not authorized' });
+  let token;
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select('-password');
-    next();
-  } catch (error) {
-    res.status(401).json({ message: 'Invalid token' });
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from DB (without password)
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next(); // allow access
+    } catch (error) {
+      res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: 'Not authorized, no token' });
   }
 };
