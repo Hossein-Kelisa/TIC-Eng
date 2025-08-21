d59c994e4a60eb0f3ebf8e7ab24f1904d659a49f
Hossein <Hossein.kelisa@gmail.com>
2025-08-21 22:12:15 +0200

    use fetch in services

---
+++ Front-End/src/services/authService.js
@@ -0,0 +1,40 @@
+export async function loginUser(data) {
+  try {
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json().catch(() => ({}));
+
+    if (!response.ok) {
+      throw new Error(result.message || "Login failed. Please try again.");
+    }
+
+    return result;
+  } catch (err) {
+    throw new Error(err.message || "Network error. Please try again later.");
+  }
+}
+
+export async function registerUser(data) {
+  try {
+    // const response = await fetch("/api/auth/register", {
+      const response = await fetch("http://localhost:5000/api/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json().catch(() => ({}));
+
+    if (!response.ok) {
+      throw new Error(result.message || "Registration failed. Try again.");
+    }
+
+    return result;
+  } catch (err) {
+    throw new Error(err.message || "Network error. Please try again later.");
+  }
+}
