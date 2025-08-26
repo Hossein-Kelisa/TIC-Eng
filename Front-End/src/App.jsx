133d38a7cef38058754eeaa819a479e4e8c1fa28
Hossein <Hossein.kelisa@gmail.com>
2025-08-26 23:26:28 +0200

    delete test loading

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -2,13 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AuthPage from "./Pages/AuthPage";
 import GlobalLoader from "./components/GlobalLoader";
-import TestLoader from "./components/TestLoader";
 
 function App() {
   return (
     <Router>
       <GlobalLoader />
-      <TestLoader />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
