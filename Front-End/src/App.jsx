e6f1c270a9072fd4ba177543c4faef3609ca18b1
Hossein <Hossein.kelisa@gmail.com>
2025-08-26 15:01:44 +0200

    wrap in main.jsx and app.jsx

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AuthPage from "./Pages/AuthPage";
+import GlobalLoader from "./components/GlobalLoader";
 
 function App() {
   return (
     <Router>
+      <GlobalLoader />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
