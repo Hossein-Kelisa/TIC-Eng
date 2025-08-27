f5eab3ff3160d85d74ede3a39bf462512d13acc9
Hossein <Hossein.kelisa@gmail.com>
2025-08-27 16:35:46 +0200

    logo in loading

--- Front-End/src/components/GlobalLoader.jsx
+++ Front-End/src/components/GlobalLoader.jsx
@@ -1,9 +1,10 @@
 import "./GlobalLoader.css";
+import logo from "../assets/logo.png";
 
 export default function GlobalLoader() {
   return (
     <div className="global-loader">
-      <div className="spinner"></div>
+      <img src={logo} alt="Logo" className="loading-logo" />
     </div>
   );
 }
