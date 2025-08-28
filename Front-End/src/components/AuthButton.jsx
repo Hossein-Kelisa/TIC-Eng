736baa291b712713930fc6f503147a33d97a4f57
Hossein <Hossein.kelisa@gmail.com>
2025-08-28 22:38:54 +0200

    auth context

--- Front-End/src/components/AuthButton.jsx
+++ Front-End/src/components/AuthButton.jsx
@@ -2,25 +2,19 @@ import { LogIn, LogOut } from "lucide-react";
 import "./AuthButton.css";
 import { useNavigate } from "react-router-dom";
 
-function AuthButton({ isLoggedIn, onLogin, onLogout }) {
+function AuthButton({ isLoggedIn, onLogout }) {
   const navigate = useNavigate();
 
   return (
     <div>
       {isLoggedIn ? (
-        <button
-          onClick={onLogout}
-          className="Auth-Logout"
-        >
+        <button onClick={onLogout} className="Auth-Logout">
           <LogOut size={22} />
           Logout
         </button>
       ) : (
         <button
-          onClick={() => {
-            onLogin();
-            navigate("/auth");
-          }}
+          onClick={() => navigate("/auth")} // just navigate to login page
           className="Auth-Login"
         >
           <LogIn size={22} />
