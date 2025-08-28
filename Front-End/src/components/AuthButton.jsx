195e166209c7bbc716f7c4efd965a2410ac98190
Hossein <Hossein.kelisa@gmail.com>
2025-08-28 22:44:06 +0200

    auth context

--- Front-End/src/components/AuthButton.jsx
+++ Front-End/src/components/AuthButton.jsx
@@ -1,20 +1,39 @@
 import { LogIn, LogOut } from "lucide-react";
 import "./AuthButton.css";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 function AuthButton({ isLoggedIn, onLogout }) {
   const navigate = useNavigate();
 
+  const handleLogout = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You will be logged out.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, logout",
+      cancelButtonText: "No, stay logged in",
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        onLogout();
+        navigate("/"); // go to home page after logout
+        Swal.fire("Logged out!", "You have successfully logged out.", "success");
+      }
+    });
+  };
+
   return (
     <div>
       {isLoggedIn ? (
-        <button onClick={onLogout} className="Auth-Logout">
+        <button onClick={handleLogout} className="Auth-Logout">
           <LogOut size={22} />
           Logout
         </button>
       ) : (
         <button
-          onClick={() => navigate("/auth")} // just navigate to login page
+          onClick={() => navigate("/auth")} // navigate to login page
           className="Auth-Login"
         >
           <LogIn size={22} />
