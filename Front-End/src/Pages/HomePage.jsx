c1362f27484b2f9adce1938ec08c03a2ee92d6bf
Hossein <Hossein.kelisa@gmail.com>
2025-08-30 22:24:21 +0200

    search

--- Front-End/src/Pages/HomePage.jsx
+++ Front-End/src/Pages/HomePage.jsx
@@ -1,13 +1,13 @@
-import TopBar from '../components/TopBar';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import Services from '../components/Services';
-import About from '../components/About';
-import Industries from '../components/Industries';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
-import Team from '../components/Team';
-import FAQ from '../components/FAQ';
+import TopBar from "../components/TopBar";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import About from "../components/About";
+import Industries from "../components/Industries";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Team from "../components/Team";
+import FAQ from "../components/FAQ";
 
 function HomePage() {
   return (
