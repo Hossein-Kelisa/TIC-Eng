966f01e54f11e158ad7749151180a783bc5b402a
Hossein <Hossein.kelisa@gmail.com>
2025-09-03 12:24:45 +0200

    request form

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AuthPage from "./Pages/AuthPage";
+import RequestForm from "./Pages/RequestForm";
 import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/auth" element={<AuthPage />} />
+          <Route path="/request" element={<RequestForm />} />
         </Routes>
       </Router>
     </AuthProvider>
