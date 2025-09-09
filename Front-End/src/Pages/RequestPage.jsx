02a05450aab336746032d92fd11f6b7d89696fc0
Hossein <Hossein.kelisa@gmail.com>
2025-09-09 21:51:32 +0200

    req page

--- Front-End/src/Pages/RequestPage.jsx
+++ Front-End/src/Pages/RequestPage.jsx
@@ -1,9 +1,15 @@
 import RequestForm from "../components/ServiceComponents/RequestForm";
+import "./RequestPage.css";
+import Header from "../components/HomeComponents/Header";
+import Footer from "../components/HomeComponents/Footer";
+import "./RequestPage.css";
 
 export default function RequestPage() {
   return (
     <div id="RequestForm">
+      <Header />
       <RequestForm />
+      <Footer />
     </div>
   );
 }
