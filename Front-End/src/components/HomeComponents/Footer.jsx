38ba67c5d88853b1515992bd1d480c345ed04576
Hossein <Hossein.kelisa@gmail.com>
2025-09-09 21:03:35 +0200

    footer

--- Front-End/src/components/HomeComponents/Footer.jsx
+++ Front-End/src/components/HomeComponents/Footer.jsx
@@ -1,9 +1,20 @@
-import './Footer.css';
+import "./Footer.css";
 
 function Footer() {
   return (
     <footer>
-      <p>&copy; 2025 Tic-Eng.com - Made with ♥ by <a href="https://HosseinKelisa.com">HosseinKelisa.com</a> | All rights reserved</p>
+      <p>
+        &copy; 2025 Tic-Eng.com - Made with ♥ by{" "}
+        <a
+          href="https://HosseinKelisa.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="HosseinKelisa.com (opens in a new tab)"
+        >
+          HosseinKelisa.com
+        </a>{" "}
+        | All rights reserved
+      </p>
     </footer>
   );
 }
