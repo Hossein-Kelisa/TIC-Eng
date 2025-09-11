df0d5c436ba847fb2fd0ce7ac781861929922a40
Hossein <Hossein.kelisa@gmail.com>
2025-09-11 22:02:28 +0200

    auth

--- Back-End/routes/authRoutes.js
+++ Back-End/routes/authRoutes.js
@@ -1,13 +1,28 @@
-import express from 'express';
-import { registerUser, loginUser, getMe } from '../controllers/authController.js';
-import { protect } from '../middlewares/authMiddleware.js';
-import { validateRegister } from '../middlewares/validateRegister.js';
-import { validateLogin } from '../middlewares/validateLogin.js';
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  getMe,
+} from "../controllers/authController.js";
+import { protect } from "../middlewares/authMiddleware.js";
+import { validateRegister } from "../middlewares/validateRegister.js";
+import { validateLogin } from "../middlewares/validateLogin.js";
 
 const authRouter = express.Router();
 
-authRouter.post('/register', validateRegister, registerUser);
-authRouter.post('/login', validateLogin, loginUser);
-authRouter.get('/me', protect, getMe);
+authRouter.post("/register", validateRegister, registerUser);
+authRouter.post("/login", validateLogin, loginUser);
+authRouter.get("/me", protect, getMe);
+authRouter.get("/verify", protect, (req, res) => {
+  // token valid → return user info
+  res.json({
+    valid: true,
+    user: {
+      _id: req.user._id,
+      userName: req.user.userName,
+      email: req.user.email,
+    },
+  });
+});
 
 export default authRouter;
