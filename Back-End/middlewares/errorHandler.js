35b19fed41b80e62fd6de4b3f05327deb3b8d6a6
Hossein <Hossein.kelisa@gmail.com>
2025-09-18 20:26:47 +0200

    update

--- Back-End/middlewares/errorHandler.js
+++ Back-End/middlewares/errorHandler.js
@@ -1,6 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  // اگر statusCode نداره، بذاریم 500
-  const statusCode = err.statusCode || 500;
+//  const statusCode = err.statusCode || 500;
   const status = err.status || "error";
 
   console.error("❌ Error:", err);
@@ -8,7 +7,7 @@ const errorHandler = (err, req, res, next) => {
   res.status(statusCode).json({
     status,
     message: err.message || "Something went wrong!",
-    // فقط در حالت توسعه stack رو نشون بده
+    //just show stack in development
     stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
   });
 };
