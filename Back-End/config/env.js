865da335566bcd7ca9b65e65aa9ff5fce8ff6cae
Hossein <Hossein.kelisa@gmail.com>
2025-09-29 21:08:54 +0200

    update

--- Back-End/config/env.js
+++ Back-End/config/env.js
@@ -1,14 +1,14 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-export const env = {
-  PORT: process.env.PORT,
-  SMTP_HOST: process.env.SMTP_HOST,
-  SMTP_PORT: process.env.SMTP_PORT,
-  SMTP_SECURE: process.env.SMTP_SECURE,
-  SMTP_USER: process.env.SMTP_USER,
-  SMTP_PASS: process.env.SMTP_PASS,
-  MAIL_FROM: process.env.MAIL_FROM,
-  MAIL_TO: process.env.MAIL_TO,
-};
+// export const env = {
+//   PORT: process.env.PORT,
+//   SMTP_HOST: process.env.SMTP_HOST,
+//   SMTP_PORT: process.env.SMTP_PORT,
+//   SMTP_SECURE: process.env.SMTP_SECURE,
+//   SMTP_USER: process.env.SMTP_USER,
+//   SMTP_PASS: process.env.SMTP_PASS,
+//   MAIL_FROM: process.env.MAIL_FROM,
+//   MAIL_TO: process.env.MAIL_TO,
+// };
