3a2139e12a48b96f6020e2740dddc84b4ca96700
Hossein <Hossein.kelisa@gmail.com>
2025-10-02 18:50:26 +0200

    update

--- Front-End/src/components/ServiceComponents/FormButton.jsx
+++ Front-End/src/components/ServiceComponents/FormButton.jsx
@@ -1,13 +1,16 @@
 import "./FormButton.css";
+import { useTranslation } from "react-i18next";
 
 export default function FormButton({ loading }) {
+  const { t } = useTranslation();
+
   return (
     <button
       type="submit"
       disabled={loading}
       className="request-form__button"
     >
-      {loading ? "Submitting..." : "Submit"}
+      {loading ? t("serviceRequest.submitLoading") : t("serviceRequest.submit")}
     </button>
   );
 }
