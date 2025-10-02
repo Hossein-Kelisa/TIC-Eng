1bd35ddf5d9e57877a04b92efcdaaf8e06701c01
Hossein <Hossein.kelisa@gmail.com>
2025-10-02 19:40:24 +0200

    update

--- Front-End/src/components/ServiceComponents/FormSelect.jsx
+++ Front-End/src/components/ServiceComponents/FormSelect.jsx
@@ -1,6 +1,7 @@
 import "./FormSelect.css";
-
+import { useTranslation } from "react-i18next";
 export default function FormSelect({ name, value, onChange, options }) {
+  const { t } = useTranslation();
   return (
     <div className="form-group">
       <select
@@ -9,7 +10,7 @@ export default function FormSelect({ name, value, onChange, options }) {
         onChange={onChange}
         className="request-form__select"
       >
-        <option value="">Select Service</option>
+        <option value="">{t("serviceRequest.formSelectService")}</option>
         {options.map((opt) => (
           <option key={opt.value} value={opt.value}>
             {opt.label}
