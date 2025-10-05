eb4e9078c1266c2828df6b391432cec8bb3ede12
Hossein <Hossein.kelisa@gmail.com>
2025-10-05 11:50:47 +0200

    add fade animation to request page

--- Front-End/src/Pages/RequestPage.jsx
+++ Front-End/src/Pages/RequestPage.jsx
@@ -2,12 +2,15 @@ import RequestForm from "../components/ServiceComponents/RequestForm";
 import Header from "../components/HomeComponents/Header";
 import Footer from "../components/HomeComponents/Footer";
 import "./RequestPage.css";
+import Fades from "../components/RestComponents/Fades";
 
 export default function RequestPage() {
   return (
     <div id="RequestForm">
       <Header />
-      <RequestForm />
+      <Fades animationType="fadeDown">
+        <RequestForm />
+      </Fades>
       <Footer />
     </div>
   );
