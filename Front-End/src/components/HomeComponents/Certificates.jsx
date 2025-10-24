d396e356deae4043f66c9bbd2bd87a87d1d0e58b
Hossein <Hossein.kelisa@gmail.com>
2025-10-24 22:55:06 +0200

    add files to section

--- Front-End/src/components/HomeComponents/Certificates.jsx
+++ Front-End/src/components/HomeComponents/Certificates.jsx
@@ -2,10 +2,10 @@ import "./Certificates.css";
 
 const Certificates = () => {
     return (
-        <div className="certificates">
+        <section className="certificates-section">
             <h2>Certificates</h2>
             <p>List of certificates will be displayed here.</p>
-        </div>
+        </section>
     );
 };
 
