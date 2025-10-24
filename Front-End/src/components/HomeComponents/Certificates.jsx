eee25e466a34e1cc1c8cc469093fa51157daa5f6
Hossein <Hossein.kelisa@gmail.com>
2025-10-24 22:51:53 +0200

    new component certificates created

---
+++ Front-End/src/components/HomeComponents/Certificates.jsx
@@ -0,0 +1,12 @@
+import "./Certificates.css";
+
+const Certificates = () => {
+    return (
+        <div className="certificates">
+            <h2>Certificates</h2>
+            <p>List of certificates will be displayed here.</p>
+        </div>
+    );
+};
+
+export default Certificates;
