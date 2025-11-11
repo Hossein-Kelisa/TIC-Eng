3a51099ae7a5b9721504ffb49b48ff6c790ce991
Hossein <Hossein.kelisa@gmail.com>
2025-11-11 18:10:39 +0100

    change layout of certificatted component

--- Front-End/src/components/HomeComponents/Certificates.jsx
+++ Front-End/src/components/HomeComponents/Certificates.jsx
@@ -1,12 +1,16 @@
 import "./Certificates.css";
 
 const Certificates = () => {
-    return (
-        <section className="certificates-section">
-            <h2>Certificates</h2>
-            <p>List of certificates will be displayed here.</p>
-        </section>
-    );
+  return (
+    <div className="certificates-container">
+      <section className="certificates-section">
+        <h2 className="certificates-title">Certificates</h2>
+        <p className="certificates-description">
+          List of our certificates will be displayed here.
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default Certificates;
