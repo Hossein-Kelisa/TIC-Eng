103dfd70f0a1a11f6036e457f3c3cce974456792
Hossein <Hossein.kelisa@gmail.com>
2025-11-11 20:40:24 +0100

    update copilot suggestions

--- Front-End/src/components/HomeComponents/Certificates.jsx
+++ Front-End/src/components/HomeComponents/Certificates.jsx
@@ -5,15 +5,11 @@ const Certificates = () => {
     <div className="certificates-container">
       <section className="certificates-section">
         <h2 className="certificates-title">Certificates</h2>
-        <div className="certificates-divider">
-          <hr />
-        </div>
+        <hr className="certificates-divider" />
         <p className="certificates-description">
           List of our certificates will be displayed here.
         </p>
-        <div className="certificates-divider">
-          <hr />
-        </div>
+        <hr className="certificates-divider" />
       </section>
     </div>
   );
