31c25ccb14626154cddcd3588c5370f70d3b33cf
Hossein <Hossein.kelisa@gmail.com>
2025-11-11 20:27:30 +0100

    update industri component with second description

--- Front-End/src/components/HomeComponents/Certificates.jsx
+++ Front-End/src/components/HomeComponents/Certificates.jsx
@@ -5,9 +5,15 @@ const Certificates = () => {
     <div className="certificates-container">
       <section className="certificates-section">
         <h2 className="certificates-title">Certificates</h2>
+        <div className="certificates-divider">
+          <hr />
+        </div>
         <p className="certificates-description">
           List of our certificates will be displayed here.
         </p>
+        <div className="certificates-divider">
+          <hr />
+        </div>
       </section>
     </div>
   );
